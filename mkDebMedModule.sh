776428e6f732f89f72cb575bb932b994ff531318
Charles Plessy <charles.plessy@oist.jp>
2021-02-09 11:06:39 +0900

    Update for Debian Med 10.7 using med-bio

--- mkDebMedModule.sh
+++ mkDebMedModule.sh
@@ -1,26 +1,27 @@
 #!/bin/bash -e
 ml singularity
 APP=$1
-DebMedVersion=DebMed9
-MODROOT=/apps/unit/BioinfoUgrp/Debian-Med
-APPDIR=$MODROOT/$DebMedVersion/$APP
-VER=$(singularity exec $MODROOT/debmed.sif dpkg-query -W -f='${Version}' $APP)
+DEBVERSION=10.7
+MODROOT=/apps/unit/BioinfoUgrp/DebianMed/$DEBVERSION
+DEBMEDIMAGE=$MODROOT/DebianMed_$DEBVERSION.sif
+APPDIR=$MODROOT/modules/$APP
+VER=$(singularity exec $DEBMEDIMAGE dpkg-query -W -f='${Version}' $APP)
 mkdir -p $APPDIR/$VER
 cd $APPDIR/$VER
 mkdir -p bin
-for prog in $($MODROOT/debmed.sif dpkg -L $APP | grep /bin/ | xargs basename -a)
+for prog in $($DEBMEDIMAGE dpkg -L $APP | grep /bin/ | xargs basename -a)
 do
 cat <<__END__ > bin/$prog
 #!/bin/sh
-LC_ALL=C singularity exec $MODROOT/debmed.sif $prog "\$@" 
+LC_ALL=C singularity exec $DEBMEDIMAGE $prog "\$@" 
 __END__
 chmod 775 bin/$prog
 done
-cd /apps/unit/BioinfoUgrp/Debian-Med/modulefiles/
+cd $MODROOT/modulefiles/
 mkdir -p $APP
-cat <<'__END__' > $APP/$VER.lua
+cat <<__END__ > $APP/$VER.lua
 -- Default settings
-local modroot    = "/apps/unit/BioinfoUgrp/Debian-Med"
+local modroot    = "$MODROOT"
 local appname    = myModuleName()
 local appversion = myModuleVersion()
 local apphome    = pathJoin(modroot, myModuleFullName())
